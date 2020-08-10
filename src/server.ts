21e68f385371cf2d611d44a3533bd380d8074605
Augusto <ahaavila@gmail.com>
2020-08-10 18:30:15 -0300

    Adicionado criação de professores, matérias e horários do professor.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
-
-app.get('/users', (request, response) => {
-  
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server rodando na porta 3333');
